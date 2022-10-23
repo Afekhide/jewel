1ba97cc20cf67e58c016212e946bce2bff806415
Dlite <afekhide009@gmail.com>
2022-10-23 23:59:00 +0100

    minor fix

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,4 +1,5 @@
 import Nav from './Nav';
+import Head from 'next/head';
 import SideNav from './SideNav';
 import AppContext from '../context/AppContext.jsx';
 import styles from '../styles/Layout.module.scss';
@@ -6,6 +7,9 @@ import styles from '../styles/Layout.module.scss';
 const Layout = ({children}) => {
   return (
       <section className={styles.container}>
+        <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200;300;400;500;600;700&display=swap" rel="stylesheet"/>
+        </Head>
         <Nav/>
         <SideNav/>
 
